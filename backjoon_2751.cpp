629bc691ba2a1442c3ef494d5cf28cad9ae22569
froh2244-wq <froh2244@gmail.com>
2026-03-25 10:15:07 +0900

    Add files via upload
    스택

--- backjoon_2751.cpp
+++ backjoon_2751.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<tuple>
 #include<algorithm>
+#include<stack>
 
 using namespace std;
 
@@ -10,21 +11,39 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N = 0;
+	int N;
 	cin >> N;
-	vector <int> v;
 
-	int temp;
-	for (int i = 0; i < N; i++)
-	{
-		cin >> temp;
-		v.push_back(temp);
-	}
-	sort(v.begin(), v.end());
-	for (int x : v) {
-		cout << x << "\n";
-	}
+	stack<int> s;
 
+	string command;
+	int num;
+	for (int i = 0; i < N; i++) {
+		cin >> command;
+		if (command == "push") {
+			cin >> num;
+			s.push(num);
+		}
+		if (command == "pop") {
+			if (!s.empty()) {
+				cout << s.top() << "\n";
+				s.pop();
+			}
+			else cout << -1 << "\n";
+		}
+		if (command == "size") {
+			cout << s.size() << "\n";
+		}
+		if (command == "empty") {
+			cout << s.empty() << "\n";
+		}
+		if (command == "top") {
+			if (!s.empty()) {
+				cout << s.top() << "\n";
+			}
+			else cout << -1 << "\n";
+		}
+	}
 	
 	
 	return 0;
